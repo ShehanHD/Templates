7373eb2f216059f306a590c0bfa6e9ae8071b02e
ShehanHD <shehanhd@gmail.com>
2021-12-08 19:56:24 +0100

    .

---
+++ next tailwind/pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from "next/head";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      ciaoo
+    </div>
+  );
+}
